ae2021c925915eae1b0ece8eb7737b430f4bcb49
井坂有花 <isakayuka@isakayukanoMacBook-Air.local>
2024-04-08 22:56:04 +0900

    [front][front]スクロールアニメーションのcss、jsの追加

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ $(".hamburger-listItem").click(function () {
   $("#icon").toggleClass("active"); //ボタンの activeクラスを除去し
   //   $(".c-hamburger__listItem").toggleClass("c-hamburger__listOpen");
 });
+
+let targets = document.querySelectorAll(".c-feature-card__img"); //アニメーションさせたい要素
+window.addEventListener("scroll", function () {
+  let scroll = window.scrollY; //スクロール量を取得
+  let windowHeight = window.innerHeight; //画面の高さを取得
+  for (let target of targets) {
+    //ターゲット要素がある分、アニメーション用のクラスをつける処理を繰り返す
+    let targetPos = target.getBoundingClientRect().top + scroll; //ターゲット要素の位置を取得
+    if (scroll > targetPos - windowHeight) {
+      //スクロール量 > ターゲット要素の位置
+      target.classList.add("fadeIn"); //クラスを加える
+    }
+  }
+});
